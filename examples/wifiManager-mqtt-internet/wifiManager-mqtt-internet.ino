d7233b14b179961e96230d4384c239506e407971
Carlos Daniel <carlos.d.san25@gmail.com>
2021-09-03 15:08:42 -0500

    cambios de topico y broker Invento

--- examples/wifiManager-mqtt-internet/wifiManager-mqtt-internet.ino
+++ examples/wifiManager-mqtt-internet/wifiManager-mqtt-internet.ino
@@ -1,15 +1,6 @@
 /*
     Control de NeoDigitos por MQTT usando WiFiManager.h
-    para configurar WiFi, broker y tópicos para suscribirse 
-    por medio de un Captive Portal. Este Captive Portal se genera
-    cada que no logra conectarse a su red wifi configurada previamente.
-
-    Está planeado para suscribirse a un tópico personalizable para el 
-    número y a otro tambien personalizable para el color a recibir.
-    El formato que tomará es "Neodigito/[TópicoPersonalizableAquí]/ESP14345274"
-    y también se suscribirá a "Neodigito/[TópicoPersonalizableAquí]" en caso de 
-    querer controlar más de un Neodigito a la vez.
-
+    para configurar por medio de un Captive Portal.
 
     Basado en las librerías de NeoDígito y WiFiManager.
     Potenciado por Inventoteca y Xircuitos, Julio, 2021.
@@ -19,7 +10,6 @@
     https://github.com/CurlyWurly-1/ESP8266-WIFIMANAGER-MQTT
 
 */
-
 #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
@@ -30,30 +20,30 @@
 #include <PubSubClient.h>
 #include <NeoDigito.h>            //https://github.com/Inventoteca/NeoDigito
 
-// Para configurar el cliente de mqtt
-WiFiClient espClient;
-PubSubClient client(espClient);
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
-char msg[MSG_BUFFER_SIZE];
-
-// Pin donde estará conectado el display
-#define PIN 0           // Para el caso especifico del ESP8266 con batería, D3 - GPIO0
-#define DIGITS 10        // Neodigitos conectados
-#define PIXPERSEG 3     // Neopixels por segmento
-
-NeoDigito display1 = NeoDigito(DIGITS, PIXPERSEG, PIN, NEO_GRB + NEO_KHZ800);
-
-WiFiManager wifiManager;
 //flag for saving data
 bool shouldSaveConfig = true;
-//define your DEFAULT values here, if there are different values in config.json, they are overwritten.
+
+//define your default values here, if there are different values in config.json, they are overwritten.
+//char mqtt_server[40];
 #define mqtt_server       "iot.inventoteca.com"
 #define mqtt_port         "1883"
 #define mqtt_user         "mqtt_user"
 #define mqtt_pass         "mqtt_pass"
-#define number_topic    "Number"
-#define color_topic    "Color"
+#define number_topic    "Neodigito/Number"
+#define color_topic    "Neodigito/Color"
+
+// Para NeoDigitos
+#define PIN 0           // Pin donde se conectará el NeoDisplay
+#define DIGITS 10        // Neodigitos conectados
+#define PIXPERSEG 3     // Neopixels por segmento
+NeoDigito display1 = NeoDigito(DIGITS, PIXPERSEG, PIN, NEO_GRB + NEO_KHZ800);
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+WiFiManager wifiManager;
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
 
 float temp = 0.0;
 float hum = 0.0;
@@ -61,60 +51,61 @@ float diff = 1.0;
 String ESP_ID = String(ESP.getChipId());
 String NumberTopicWithESP_ID;
 String ColorTopicWithESP_ID;
-String NumberTopic;
-String ColorTopic;
-String literalementeNada = "";
-//Porque la librería WM los quiere como arreglos de caracteres
-char charColorTopicESP_ID[100];
-char charNumberTopicESP_ID[100];
-char charColorTopic[100];
-char charNumberTopic[100];
 
 void setup()
 {
-  display1.begin(); // Esta función llama Adafruit_NeoPixel.begin() para configurar;
-  display1.setPixelColor(0x090000);
   Serial.begin(115200);
+  Serial.println();
+  //Neodigitos
+  display1.begin();
+  display1.setPixelColor(0x090000);
+  display1.print("A");
+  display1.show();
+  Serial.println("NeoDisplay iniciando.");
+  delay(3000);
+
+  //clean FS for testing
+  //SPIFFS.format(); //<--COMENTAR DESPUÉS DE TESTEAR
+  checkSPIFFS();
+ 
 
   // Para que el Captive Portal también pida esta info
   // id/name placeholder/prompt default length
   WiFiManagerParameter custom_mqtt_server("server", "MQTT Server", mqtt_server, 40);
   WiFiManagerParameter custom_mqtt_port("port", "MQTT Port", mqtt_port, 6);
-  WiFiManagerParameter custom_number_topic("number", "Topico NeoNumber: Neodigito/ ... /ESPXXXX", number_topic, 40);
-  WiFiManagerParameter custom_color_topic("color", "Topico NeoColor: Neodigito/ ... /ESPXXXX", color_topic, 40);
+  WiFiManagerParameter custom_number_topic("number", "Topico NeoColor: ESPXXXX/", number_topic, 40); 
+  WiFiManagerParameter custom_color_topic("color", "Topico NeoColor: ESPXXXX/", color_topic, 40);
+
+  // Reset Wifi settings for testing
+  //wifiManager.resetSettings(); //<--COMENTAR DESPUÉS DE TESTEAR
+
+
+  //set static ip
+  //  wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
   //add all your parameters here
   wifiManager.addParameter(&custom_mqtt_server);
   wifiManager.addParameter(&custom_mqtt_port);
   wifiManager.addParameter(&custom_number_topic);
   wifiManager.addParameter(&custom_color_topic);
 
-  // Reset Wifi settings for testing
-  //wifiManager.resetSettings(); //<--COMENTAR DESPUÉS DE TESTEAR
-  //clean FS for testing
-  //SPIFFS.format(); //<--COMENTAR DESPUÉS DE TESTEAR
 
-  //Revisa si ya hay config de parámetros extra guardada previamente
-  checkSPIFFS();
-  
   //Intenta conectarse a WiFi, si fracasa genera AP
   //y se queda en bucle esperando a la nueva configuración
-  //wifiManager.autoConnect(); //Alternativamente: wifiManager.autoConnect("NombreAP_Aqui","password");
-  wifiManager.autoConnect("Neodigito","12345678");
-  
+  wifiManager.autoConnect();
+
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
 
   //read updated parameters
   strcpy(mqtt_server, custom_mqtt_server.getValue());
   strcpy(mqtt_port, custom_mqtt_port.getValue());
-  strcpy(number_topic, custom_number_topic.getValue());
+  strcpy(number_topic, custom_number_topic.getValue()); 
   strcpy(color_topic, custom_color_topic.getValue());
 
   if (shouldSaveConfig) saveFS();
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  
+  Serial.println("Local ip: ");
   Serial.println(WiFi.localIP());
   display1.print("Internet");
   display1.show();
@@ -123,109 +114,65 @@ void setup()
   display1.show();
   delay(500);
 
-  //Para poder controlar individualmente cada neodigito
-  NumberTopicWithESP_ID = literalementeNada + "Neodigito/" + number_topic + "/ESP" + ESP_ID;
-  ColorTopicWithESP_ID = literalementeNada + "Neodigito/" + color_topic + "/ESP" + ESP_ID;
-  //Para poder controlar grupalmente los neodigitos
-  NumberTopic = literalementeNada + "Neodigito/" + number_topic;
-  ColorTopic = literalementeNada + "Neodigito/" + color_topic;
-    
-  NumberTopicWithESP_ID.toCharArray(charNumberTopicESP_ID, 100);
-  ColorTopicWithESP_ID.toCharArray(charColorTopicESP_ID, 100);
-  NumberTopic.toCharArray(charNumberTopic, 100);
-  ColorTopic.toCharArray(charColorTopic, 100);
-    
-  Serial.print("Esperando mensajes en: ");
-  Serial.println(charNumberTopicESP_ID);
-  Serial.print("Y tambien en: ");
-  Serial.println(charColorTopicESP_ID);
-  
-  int  mqtt_port_int;
-  mqtt_port_int = atoi(mqtt_port);
-  client.setServer(mqtt_server, mqtt_port_int);
+  const uint16_t mqtt_port_x = 1883; //<-----------------------------------CORREGIR
+  client.setServer(mqtt_server, mqtt_port_x);
   client.setCallback(callback);
+
+
+  NumberTopicWithESP_ID = "ESP" + ESP_ID + "/" + number_topic;
+  ColorTopicWithESP_ID = "ESP" + ESP_ID + "/" + color_topic;
+  Serial.print("Esperando mensajes en: ");
+  Serial.println(NumberTopicWithESP_ID);
+  Serial.print("Y tambien en: ");
+  Serial.println(ColorTopicWithESP_ID);
+
+  char topic[100];
+
+  float newTemp = 10;
+  float newHum = 20;
+  hum = newHum;
+
+//  Serial.print("Publicando cosas a: ");
+//  snprintf(topic, sizeof(topic), "ESP%s/%s", ESP_ID, number_topic);
+//  Serial.println(topic);
+//  client.publish(topic, String(hum).c_str(), true);
 }
 
-void loop()
-{
-
-  if (!client.connected())
-  {
+//-----------------------------------------LOOP()---------------------------
+void loop() {
+  //Para MQTT
+  if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
+  long now = millis();
+  if (now - lastMsg > 3000) { //Cada segundo
+    lastMsg = now;
+    char topic[100];
+    char payload[100];
+    float newTemp = 10;
+    float newHum = 20;
+    hum = newHum;
+
+    Serial.print("Publicando cosas a: ");
+    snprintf(topic, sizeof(topic), "ESP%s/%s", ESP_ID, number_topic);
+    Serial.println(topic);
+    snprintf(payload, sizeof(payload), "%.2f", hum);
+    client.publish(topic, payload);
+  }
 }
 
-
-
-// -------------------------------------------- callback
-// aqui se lee el mensaje letra por letra
-void callback(char* topic, byte* payload, unsigned int length)
-{
-  String StrColor = "";
-  String StrTopic = topic;
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.println("] ");
-
-  
-    if (StrTopic == charColorTopicESP_ID|| StrTopic == charColorTopic) 
-  {
-    for (int i = 0; i < length; i++)
-    {
-      StrColor += (char)payload[i];
-
-    }
-    display1.setPixelColor(StrColor.toInt());
-    //display1.print(StrColor);
-    display1.show();
-    Serial.print(StrColor);
-    StrColor = "";
-  }
-
-  else if (StrTopic == charNumberTopicESP_ID || StrTopic == charNumberTopic) 
-  {
-    display1.clear();
-    for (int i = 0; i < length; i++)
-    {
-      Serial.print((char)payload[i]);
-      StrColor += (char)payload[i];
-      //display1.write((char)payload[i],i);
-    }
-
-    display1.print(StrColor);
-    Serial.println();
-    display1.show();
-  }
-
-
-}
-
-
-// ----------------------------------------- reconect
-void reconnect()
-{
+//-----------------------------------------OTRAS FUNCIONES---------------------------
+void reconnect() {
+  char esp_id[20];
+  snprintf(esp_id, sizeof(esp_id), "%s", ESP_ID);
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str()))
-    {
+    if (client.connect(esp_id)) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("neoAlive", charNumberTopicESP_ID);
-      // ... and resubscribe
-      client.subscribe(charNumberTopicESP_ID);
-      client.subscribe(charColorTopicESP_ID);
-      client.subscribe(charNumberTopic);
-      client.subscribe(charColorTopic);
-    }
-    else
-    {
+    } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -235,9 +182,54 @@ void reconnect()
   }
 }
 
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+// -------------------------------------------- callback
+// aqui se lee el mensaje letra por letra
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  String StrDisplay = "";
+  String StrTopic = topic;
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  if (StrTopic == ColorTopicWithESP_ID)
+  {
+    for (int i = 0; i < length; i++)
+    {
+      StrDisplay += (char)payload[i];
+
+    }
+    display1.setPixelColor(StrDisplay.toInt());
+    display1.show();
+    Serial.print(StrDisplay);
+    StrDisplay = "";
+  }
+  else if (StrTopic == NumberTopicWithESP_ID)
+  {
+    display1.clear();
+    for (int i = 0; i < length; i++)
+    {
+      Serial.print((char)payload[i]);
+      StrDisplay += (char)payload[i];
+    }
+
+    display1.print(StrDisplay);
+    Serial.println();
+    display1.show();
+  }
+
+}
+
 void checkSPIFFS()
 {
-  //read configuration from FS json
+   //read configuration from FS json
   Serial.println("mounting FS...");
 
   if (SPIFFS.begin()) { //Revisa si existe configuración previa
